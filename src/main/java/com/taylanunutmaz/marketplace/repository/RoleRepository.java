aee7ba7e1fa6d647baf124b8afff9b3a82010092
Taylan Unutmaz <taylanunutmaz@gmail.com>
2021-01-23 22:19:10 +0300

    Add UserRepository and RoleRepository repositories

---
+++ src/main/java/com/taylanunutmaz/marketplace/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.taylanunutmaz.marketplace.repository;
+
+import com.taylanunutmaz.marketplace.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com/taylanunutmaz/marketplace/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.taylanunutmaz.marketplace.repository;
+
+import com.taylanunutmaz.marketplace.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
