3140cbb6b5a7e020dfb212e1dceaf67f9b08b53c
Taylan Unutmaz <taylanunutmaz@gmail.com>
2021-01-23 21:35:17 +0300

    Add User and Role models

---
+++ src/main/java/com/taylanunutmaz/marketplace/model/Role.java
@@ -0,0 +1,17 @@
+package com.taylanunutmaz.marketplace.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+}
