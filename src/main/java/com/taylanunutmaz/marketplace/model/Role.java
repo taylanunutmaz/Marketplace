b16a58011ec5d43903e9c838400ceb42a9ac326c
Taylan Unutmaz <taylanunutmaz@gmail.com>
2021-01-23 22:29:25 +0300

    Add UserDetailsServiceImpl

--- src/main/java/com/taylanunutmaz/marketplace/model/Role.java
+++ src/main/java/com/taylanunutmaz/marketplace/model/Role.java
@@ -14,4 +14,28 @@ public class Role {
 
     @ManyToMany(mappedBy = "roles")
     private Set<User> users;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
 }
