7b8621744a73532bdf866ca0f4969f6480f9836b
Taylan Unutmaz <taylanunutmaz@gmail.com>
2021-01-23 23:29:21 +0300

    Add UserService

---
+++ src/main/java/com/taylanunutmaz/marketplace/service/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.taylanunutmaz.marketplace.service;
+
+import com.taylanunutmaz.marketplace.model.User;
+import com.taylanunutmaz.marketplace.repository.RoleRepository;
+import com.taylanunutmaz.marketplace.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.HashSet;
+
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(new HashSet<>(roleRepository.findAll()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
